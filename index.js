74a62cc2c6538cb9828c19a2be5fb41d0f694673
Yuu YABU <wongty0220@gmail.com>
2023-02-15 17:08:44 +0900

    HTTPサーバにアクセスする際ログを残すように実装

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 const http = require("http");
 const server = http.createServer((req, res) => {
+  console.info(
+    `[${new Date()}] Requested by ${req.socket.remoteAddress}`
+  );
   res.writeHead(200, {
     "Content-type": "text/plain; charset=utf-8",
   });
